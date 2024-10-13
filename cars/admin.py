97fb4c6617696ac4f1fe6d84a1e2933f39ebd5fa
Matheus <matheushenriquesilva.th@gmail.com>
2024-10-13 18:16:30 -0300

    Painel Admin: exibindo a model car no painel

--- cars/admin.py
+++ cars/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from cars.models import Car
 
-# Register your models here.
+class CarAdmin(admin.ModelAdmin):
+    list_display = ("model", "brands", "factory_year", "model_year", "value")
+    search_fields = ("model", "factory_year")
+
+admin.site.register(Car, CarAdmin)
