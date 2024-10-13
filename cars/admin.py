47bf702bc84c05859aa895c65ce8a6819c10215c
Matheus <matheushenriquesilva.th@gmail.com>
2024-10-13 18:31:29 -0300

    Deixando o painel amigavel

--- cars/admin.py
+++ cars/admin.py
@@ -3,6 +3,6 @@ from cars.models import Car
 
 class CarAdmin(admin.ModelAdmin):
     list_display = ("model", "brands", "factory_year", "model_year", "value")
-    search_fields = ("model", "factory_year")
+    search_fields = ("model", "brands")
 
 admin.site.register(Car, CarAdmin)
